7e09c198b9577edfa1e3ecb063b1e7020efe06ab
Jon-Erik <jdc151@case.edu>
2018-02-27 16:27:17 -0500

    basically finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,7 +14,9 @@ var questions = ["Johann Sebastian Bach, Domenico Scarlatti, and Georg Frederick
 				 "How many children did Johann Sebastian Bach have?",
 				 "Which Beethoven symphony includes choir and vocal soloists?",
 				 "Which Stravinsky ballet famously elicited a riot?",
-				 "Several important 20th century composers moved to which American city in the 1930s and 40s?"]
+				 "Several important 20th century composers moved to which American city in the 1930s and 40s?",
+				 "How old was Mozart when he died?",
+				 "Which composer was also an artist who painter watercolours?"]
 
 var choices = [
 				["1685", "1701", "1680", "1709"],
@@ -23,19 +25,23 @@ var choices = [
 				"Symphony No. 9 in D minor"],
 				["The Firebird", "The Rite of Spring", "Petrushka", "Pulcinella"],
 				["Chicago", "New York", "Los Angeles", "Seattle"],
+				["57", "63", "29", "35"],
+				["Gustav Mahler", "Richard Strauss", "Felix Mendelssohn", "Ludwig van Beethoven"]
 			  ]
 
-var correctAnswers = ["1685", "20", "Symphony No. 9 in D minor", "The Rite of Spring", "Los Angeles"];
+var correctAnswers = ["1685", "20", "Symphony No. 9 in D minor", "The Rite of Spring", "Los Angeles", "35", "Felix Mendelssohn"];
 
 var explanations = ["Bach, Scarlatti, and Handel were all born in 1685, sometimes being referred to as 'The Class of 1685.'",
 				    "Bach was married twice, to Maria Barbara and Anna Magdalena, with whom he had 20 children. Sadly only 10 reached adulthood.",
 				    "Beethoven's ninth symphony was revolutionary, not only in its length and scope, but in its use of choir and vocal soloists in the last movement.",
 				    "Stravinsky's The Rite of Spring (Le Sacre du Printemps) caused a riot at its premiere at Les Ballets Russes in Paris in 1913.",
-				    "Mostly because of the dangerous political situation at the time in Europe, composers such as Stravinsky, Schoenberg, and many other musicians moved to Los Angeles in the 1930s and 40s."];
+				    "Mostly because of the dangerous political situation at the time in Europe, composers such as Stravinsky, Schoenberg, and many other musicians moved to Los Angeles in the 1930s and 40s.",
+				    "Mozart only lived to be 35 years old, but not before writing numerous symphonies, operas, religious works, and a great deal of chamber music, among other compositions.",
+				    "Mendelssohn was an art enthusiast and would frequently paint watercolour pictures."];
 
 function countDownQuestion() {
 		questionTime--;
-		$("#time-remaining").text("Time Remaining: " + questionTime + " seconds");
+		$("#time-remaining").html("</br>Time Remaining: " + questionTime + " seconds");
 		if (questionTime === 0) {
 			clearInterval(intervalIdQuestion);
 			questionTime = 5;
@@ -52,7 +58,7 @@ function displayRemainingQuestion() {
 function countDownAnswer() {
 	answerTime--;
 	//questionCount++;
-	$("#time-remaining").text("Time Remaining: " + answerTime + " seconds");
+	$("#time-remaining").html("</br>Time Remaining: " + answerTime + " seconds");
 	if (answerTime === 0) {
 		clearInterval(intervalIdAnswer);
 		answerTime = 5;
@@ -69,7 +75,7 @@ function displayRemainingAnswer() {
 
 function displayQuestion(questionCount) {
 	if (questionCount === questions.length) {
-		$("#question-answer").html("The end. Correct Answers: " + correctResponses + "</br>" +
+		$("#question-answer").html("Thanks for playing! Here's your score:</br></br> Correct Answers: " + correctResponses + "</br>" +
 									"Incorrect Answers: " + incorrectResponses + "</br>" + 
 									"Unanswered Questions: " + unansweredQuestions);
 		$("#time-remaining").hide();
@@ -77,57 +83,48 @@ function displayQuestion(questionCount) {
 	}
 
 	$("#question-answer").html(questions[questionCount] + "</br></br>" + 
-		"<button class='btn choice'>" + choices[questionCount][0] + "</button>" + "</br></br>" +
-		"<button class='btn choice'>" + choices[questionCount][1] + "</button>" + "</br></br>" +
-		"<button class='btn choice'>" + choices[questionCount][2] + "</button>" + "</br></br>" +
+		"<button class='btn choice'>" + choices[questionCount][0] + "</button>" + "    " + 
+		"<button class='btn choice'>" + choices[questionCount][1] + "</button>" + "    " +
+		"<button class='btn choice'>" + choices[questionCount][2] + "</button>" + "    " +
 		"<button class='btn choice'>" + choices[questionCount][3] + "</button>");
 	
 	$(".choice").on("click", function () {
 		var selectedAnswer = $(this).text();
 		if (selectedAnswer === correctAnswers[questionCount]) {
-			displayAnswerCorrect();
+			displayAnswerCorrect(questionCount);
 		} else {
-			displayAnswerIncorrect();
+			displayAnswerIncorrect(questionCount);
 		}
 	})
 
-	$("#time-remaining").text("Time Remaining: " + questionTime + " seconds");
+	$("#time-remaining").html("</br>Time Remaining: " + questionTime + " seconds");
 	displayRemainingQuestion();
 }
 
 function displayAnswerCorrect(questionCount) {
 	clearInterval(intervalIdQuestion);
 	correctResponses++;
-	console.log(correctResponses);
-	$("#question-answer").html("Correct! The answer is: " + correctAnswers[questionCount] +
-	  "</br>" + explanations[questionCount]);
-	console.log(correctAnswers[questionCount]);
-	console.log(explanations[questionCount]);
-	$("#time-remaining").text("Time Remaining: " + answerTime + " seconds");
+	$("#question-answer").html("Correct!</br> The answer is: " + correctAnswers[questionCount] +
+	  "</br></br>" + explanations[questionCount]);
+	$("#time-remaining").html("</br>Time Remaining: " + answerTime + " seconds");
 	displayRemainingAnswer();
 }
 
 function displayAnswerIncorrect(questionCount) {
 	clearInterval(intervalIdQuestion);
 	incorrectResponses++;
-	console.log(incorrectResponses);
-	$("#question-answer").html("Incorrect! The correct answer is: " + correctAnswers[questionCount] +
-	  "</br>" + explanations[questionCount]);
-	console.log(correctAnswers[questionCount]);
-	console.log(explanations[questionCount]);
-	$("#time-remaining").text("Time Remaining: " + questionTime + " seconds");
+	$("#question-answer").html("Incorrect!</br> The correct answer is: " + correctAnswers[questionCount] +
+	  "</br></br>" + explanations[questionCount]);
+	$("#time-remaining").html("</br>Time Remaining: " + questionTime + " seconds");
 	displayRemainingAnswer();
 }
 
 function displayAnswerUnanswered(questionCount) {
 	clearInterval(intervalIdQuestion);
 	unansweredQuestions++;
-	console.log(unansweredQuestions);
-	$("#question-answer").html("The correct answer is: " + correctAnswers[questionCount] +
-	  "</br>" + explanations[questionCount]);
-	console.log(correctAnswers[questionCount]);
-	console.log(explanations[questionCount]);
-	$("#time-remaining").text("Time Remaining: " + questionTime + " seconds");
+	$("#question-answer").html("Time's Up!</br>The answer is: " + correctAnswers[questionCount] +
+	  "</br></br>" + explanations[questionCount]);
+	$("#time-remaining").html("</br>Time Remaining: " + questionTime + " seconds");
 	displayRemainingAnswer();
 }
 
